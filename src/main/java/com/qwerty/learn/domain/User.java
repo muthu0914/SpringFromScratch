3629de9cabbd631d1965d7ae4a156765c476b0ba
muthu0914 <muthu.rk0914@gmail.com>
2024-01-24 04:00:13 +0530

    JWT Authorization with UserDetails Implemented

---
+++ src/main/java/com/qwerty/learn/domain/User.java
@@ -0,0 +1,92 @@
+package com.qwerty.learn.domain;
+
+import java.util.Collection;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "user")
+public class User implements UserDetails{
+
+	private static final long serialVersionUID = -1397421022451314399L;
+	@Id
+	@Type(type = "uuid-char")
+	private UUID id;
+	@Column(name = "userName")
+	private String userName;
+	@Column(name = "password")
+	private String password;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	public UserDetails orElseThrow(Object object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
